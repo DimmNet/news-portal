5b85ae610d6d8e054be352de118262f0a1dea1f7
DimmNet <30796320+DimmNet@users.noreply.github.com>
2017-09-04 13:37:39 +0300

    Заготовка для новостей

---
+++ app/News.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'body', 'password',
+    ];
+
+    /**
+     * Получить создателя новости.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
