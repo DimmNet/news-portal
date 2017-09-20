6540d669506a6811832bc8beeef5a752ce66581d
DimmNet <30796320+DimmNet@users.noreply.github.com>
2017-09-20 13:32:31 +0300

    Исправил выход

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,7 +5,15 @@
 
             @if (Auth::check())
                 <a class="nav-link{{ route::is('news.create') ? ' active' : '' }}" href="{{ route('news.create') }}">@lang('layouts.create_news')</a>
-                <a class="nav-link ml-auto" href="{{ route('logout') }}">{{ Auth::user()->name }}</a>
+                <a class="nav-link ml-auto" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    {{ Auth::user()->name }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
             @else
                 <a class="nav-link{{ route::is('login') ? ' active' : '' }} ml-auto" href="{{ route('login') }}">@lang('auth.entry')</a>
                 <a class="nav-link{{ route::is('register') ? ' active' : '' }}" href="{{ route('register') }}">@lang('auth.register')</a>
